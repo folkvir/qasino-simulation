2f8586aef72a94385eadc9def143d0f92a8604bb
Grall Arnaud <folkvir@outlook.fr>
2018-11-18 23:22:42 +0100

    update xp script

--- xp.sh
+++ xp.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+rm -rf xp.log
 sh install.sh
-
-java -Xms500000m -javaagent:target/snob.jar -jar target/snob.jar snob-50-rps+son.txt
-java -Xms500000m -javaagent:target/snob.jar -jar target/snob.jar snob-50-rps.txt
-java -Xms500000m -javaagent:target/snob.jar -jar target/snob.jar snob-100-rps+son.txt
-java -Xms500000m -javaagent:target/snob.jar -jar target/snob.jar snob-100-rps.txt
-java -Xms500000m -javaagent:target/snob.jar -jar target/snob.jar snob-200-rps+son.txt
-java -Xms500000m -javaagent:target/snob.jar -jar target/snob.jar snob-200-rps.txt
+LOG="xp.log"
+HEAP="-Xms500000m"
+JAR="-jar target/snob.jar"
+xps=(snob-50-rps+son.txt snob-50-rps.txt snob-50-rps.txt snob-100-rps+son.txt snob-100-rps.txt snob-200-rps+son.txt snob-200-rps.txt)
+for xp in ${xps[*]}
+do
+    echo "Running: " $xp
+    java $(HEAP) $(JAR) $xp > $(LOG)
+done
 wait
