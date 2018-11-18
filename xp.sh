c2704b942f4318c39f59a5eb1bfcc5027a436163
Grall Arnaud <folkvir@outlook.fr>
2018-11-18 17:02:59 +0100

    100go memory for the xp

--- xp.sh
+++ xp.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 sh install.sh
 
-java -javaagent:target/snob.jar -jar target/snob.jar snob-50-rps+son.txt
-java -javaagent:target/snob.jar -jar target/snob.jar snob-50-rps.txt
-java -javaagent:target/snob.jar -jar target/snob.jar snob-100-rps+son.txt
-java -javaagent:target/snob.jar -jar target/snob.jar snob-100-rps.txt
-java -javaagent:target/snob.jar -jar target/snob.jar snob-200-rps+son.txt
-java -javaagent:target/snob.jar -jar target/snob.jar snob-200-rps.txt
+java -Xms100000m -javaagent:target/snob.jar -jar target/snob.jar snob-50-rps+son.txt
+java -Xms100000m -javaagent:target/snob.jar -jar target/snob.jar snob-50-rps.txt
+java -Xms100000m -javaagent:target/snob.jar -jar target/snob.jar snob-100-rps+son.txt
+java -Xms100000m -javaagent:target/snob.jar -jar target/snob.jar snob-100-rps.txt
+java -Xms100000m -javaagent:target/snob.jar -jar target/snob.jar snob-200-rps+son.txt
+java -Xms100000m -javaagent:target/snob.jar -jar target/snob.jar snob-200-rps.txt
 wait
