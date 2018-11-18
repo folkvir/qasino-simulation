6d69c6ca856373f8ee227ecddd220ea67940ff93
Grall Arnaud <folkvir@outlook.fr>
2018-11-18 23:47:47 +0100

    fix xp script

--- xp.sh
+++ xp.sh
@@ -3,13 +3,13 @@
 LOG="xp.log"
 HEAP="-Xms500000m"
 JAR="-jar target/snob.jar"
-xps[0]=snob-50-rps+son.txt
-xps[1]=snob-50-rps.txt
-xps[2]=snob-50-rps.txt
-xps[3]=snob-100-rps+son.txt
-xps[4]=snob-100-rps.txt
-xps[5]=snob-200-rps+son.txt
-xps[6]=snob-200-rps.txt
+xps[0]="snob-50-rps+son.txt"
+xps[1]="snob-50-rps.txt"
+xps[2]="snob-50-rps.txt"
+xps[3]="snob-100-rps+son.txt"
+xps[4]="snob-100-rps.txt"
+xps[5]="snob-200-rps+son.txt"
+xps[6]="snob-200-rps.txt"
 for xp in ${xps[*]}
 do
     echo "Running: " $xp
