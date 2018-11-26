8bc20f858433cc56843e7132c510dd3b46d370d1
Grall Arnaud <folkvir@outlook.fr>
2018-11-26 16:58:02 +0100

    optimize, do not ask again to the same peer, if the same peer is in the son

--- src/main/java/snob/simulation/snob2/Snob.java
+++ src/main/java/snob/simulation/snob2/Snob.java
@@ -166,17 +166,13 @@ public class Snob extends ARandomPeerSamplingProtocol implements IRandomPeerSamp
             // 1 - send tpqs to neighbours and receive responses
             List<Node> rps_neigh = this.getPeers(1000000);
             for (Node node_rps : rps_neigh) {
-                if (this.profile.has_query && this.profile.query.patterns.size() > 0) {
-                    Snob snob = (Snob) node_rps.getProtocol(ARandomPeerSamplingProtocol.pid);
-                    this.exchangeTriplePatterns(snob);
-                }
+                this.exchangeTriplePatterns((Snob) node_rps.getProtocol(ARandomPeerSamplingProtocol.pid));
             }
             if (Snob.son && this.isUp() && this.sonPartialView.size() > 0) {
                 List<Node> son_neigh = this.getSonPeers(1000000);
                 for (Node node_son : son_neigh) {
-                    if (this.profile.has_query && this.profile.query.patterns.size() > 0) {
-                        Snob snob = (Snob) node_son.getProtocol(ARandomPeerSamplingProtocol.pid);
-                        this.exchangeTriplePatterns(snob);
+                    if(!rps_neigh.contains(node_son)) {
+                        this.exchangeTriplePatterns((Snob) node_son.getProtocol(ARandomPeerSamplingProtocol.pid));
                     }
                 }
             }
