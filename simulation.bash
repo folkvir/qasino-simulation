f2e2e0182f0be34d2af9ac8e754ae4bc9b0def43
Grall Arnaud <folkvir@outlook.fr>
2018-11-28 15:45:58 +0100

    update sim bash

--- simulation.bash
+++ simulation.bash
@@ -5,21 +5,23 @@ SAMPLE=10
 K=6
 SON=0
 
-java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "simk-${k}size-${SIZE}-sample${SAMPLE}.log"
+mkdir -p ./results/simulation
+
+java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "./results/simulation/simk-${K}-son-${SON}-size-${SIZE}-sample${SAMPLE}.log"
 
 SON=1
-java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "simk-${k}size-${SIZE}-sample${SAMPLE}.log"
+java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "./results/simulation/simk-${K}-son-${SON}-ize-${SIZE}-sample${SAMPLE}.log"
 
 SON=2
-java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "simk-${k}size-${SIZE}-sample${SAMPLE}.log"
+java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "./results/simulation/simk-${K}-son-${SON}-size-${SIZE}-sample${SAMPLE}.log"
 
 SON=3
-java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "simk-${k}size-${SIZE}-sample${SAMPLE}.log"
+java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "./results/simulation/simk-${K}-son-${SON}-size-${SIZE}-sample${SAMPLE}.log"
 
 SON=4
-java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "simk-${k}size-${SIZE}-sample${SAMPLE}.log"
+java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "./results/simulation/simk-${K}-son-${SON}-size-${SIZE}-sample${SAMPLE}.log"
 
 SON=5
-java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "simk-${k}size-${SIZE}-sample${SAMPLE}.log"
+java -cp target/classes/ snob.simulation.Simulation $SIZE $SAMPLE $K $SON > "./results/simulation/sim-k-${K}-son-${SON}-size-${SIZE}-sample${SAMPLE}.log"
 
 wait
