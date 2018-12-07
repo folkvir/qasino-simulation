dc136c7d1717b3d41102b1b7db52eabb0e263e6d
Grall Arnaud <folkvir@outlook.fr>
2018-12-07 23:42:06 +0100

    add variationsnq in bash script

---
+++ variations.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+bash install.bash
+
+JAR="-jar target/snob.jar"
+SAMPLE=100
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    DIRNAME=`date | md5sum | cut -d' ' -f1`
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    DIRNAME=`date | md5`
+fi
+
+DIR="./results/${DIRNAME}variancenq"
+mkdir -p $DIR
+
+java -cp target/classes/ snob.simulation.VariationNQ > "${DIR}/variationnsnq.csv"
