47f9dd9ee16e93607d9bb89271ba38b1035ee601
Grall Arnaud <folkvir@outlook.fr>
2019-03-28 15:29:00 +0100

    remove to 100

--- scripts/spray.bash
+++ scripts/spray.bash
@@ -5,7 +5,7 @@ CUR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo $CUR
 
 JAR="-jar target/snob.jar"
-HEAP="-Xms50g -Xmx50g"
+HEAP="-Xms100g -Xmx100g"
 
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
